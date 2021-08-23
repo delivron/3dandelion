3b715ff7fc0d5a18a59734aac105e1060e0aa0ff
delivron <sh.delivron@ya.ru>
2021-08-23 22:13:30 +0300

    Add command list

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,7 @@
 #include "window.h"
 
 #include <wrl.h>
-#include <d3d12.h>
+#include <d3dx12.h>
 #include <dxgi1_4.h>
 
 #include <stdexcept>
@@ -45,6 +45,40 @@ int main()
         swap_chain_desc.SampleDesc.Count = 1;
         ThrowIfFailed(factory->CreateSwapChainForHwnd(queue.Get(), window_handle, &swap_chain_desc, nullptr, nullptr, &swap_chain));
     }
+
+    ComPtr<ID3D12DescriptorHeap> descriptor_heap;
+    {
+        D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+        desc.NumDescriptors = 1;
+        desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        ThrowIfFailed(device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&descriptor_heap)));
+    }
+
+    ComPtr<ID3D12Resource> back_buffer;
+    ThrowIfFailed(swap_chain->GetBuffer(0, IID_PPV_ARGS(&back_buffer)));
+    device->CreateRenderTargetView(back_buffer.Get(), nullptr, descriptor_heap->GetCPUDescriptorHandleForHeapStart());
+
+    ComPtr<ID3D12CommandAllocator> command_allocator;
+    ThrowIfFailed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&command_allocator)));
+
+    ComPtr<ID3D12GraphicsCommandList> command_list;
+    ThrowIfFailed(device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, command_allocator.Get(), nullptr, IID_PPV_ARGS(&command_list)));
+    {
+        auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(back_buffer.Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+        command_list->ResourceBarrier(1, &barrier);
+    }
+    {
+        const float color[4] = { 0.0f, 0.5f, 1.0f, 1.0f };
+        command_list->ClearRenderTargetView(descriptor_heap->GetCPUDescriptorHandleForHeapStart(), color, 0, nullptr);
+    }
+    {
+        auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(back_buffer.Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+        command_list->ResourceBarrier(1, &barrier);
+    }
+    command_list->Close();
+
+    ID3D12CommandList* command_lists[] = { command_list.Get() };
+    queue->ExecuteCommandLists(static_cast<UINT>(std::size(command_lists)), command_lists);
 
     swap_chain->Present(0, 0);
 
