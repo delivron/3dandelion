923c2cbfcde7178a33f170451825321104379178
delivron <sh.delivron@ya.ru>
2021-12-16 22:34:29 +0300

    Add helpers

--- source/utils.cpp
+++ source/utils.cpp
@@ -1,6 +1,27 @@
 #include "utils.h"
 
+#include <d3dcompiler.h>
+
+#include <sstream>
 #include <stdexcept>
+#include <type_traits>
+
+using namespace Microsoft::WRL;
+
+namespace
+{
+
+template <typename T, typename = std::enable_if_t<std::is_integral_v<T>, T>>
+T CreateFlags(T initial_flags, T debug_flag)
+{
+    auto flags = initial_flags;
+#ifdef _DEBUG
+    flags |= debug_flag;
+#endif
+    return flags;
+}
+
+}
 
 namespace ddn
 {
@@ -10,6 +31,39 @@ void ValidateResult(HRESULT hr)
     if (FAILED(hr)) {
         throw std::runtime_error("Invalid return value");
     }
+}
+
+ComPtr<ID3DBlob> CompileShader(const std::string& source, const std::string& entry_point, const std::string& compiler_target)
+{
+    ComPtr<ID3DBlob> shader;
+    ComPtr<ID3DBlob> error;
+    auto compiler_flags = CreateFlags<UINT>(0, D3DCOMPILE_DEBUG);
+    auto hr = D3DCompile(source.data(), source.size(), nullptr, nullptr, nullptr, entry_point.c_str(), compiler_target.c_str(), compiler_flags, 0, &shader, &error);
+
+    if (FAILED(hr)) {
+        std::wostringstream oss;
+        oss << "Compilation error: " << static_cast<char*>(error->GetBufferPointer()) << std::endl;
+        OutputDebugString(oss.str().c_str());
+    }
+
+    ValidateResult(hr);
+
+    return shader;
+}
+
+ComPtr<IDXGIFactory5> CreateFactory()
+{
+    ComPtr<IDXGIFactory5> factory;
+    auto factory_flags = CreateFlags<UINT>(0, DXGI_CREATE_FACTORY_DEBUG);
+    ValidateResult(CreateDXGIFactory2(factory_flags, IID_PPV_ARGS(&factory)));
+    return factory;
+}
+
+ComPtr<ID3D12Device> GetDevice(ID3D12DeviceChild& device_child)
+{
+    ComPtr<ID3D12Device> device;
+    device_child.GetDevice(IID_PPV_ARGS(&device));
+    return device;
 }
 
 }  // namespace ddn
--- source/utils.h
+++ source/utils.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#include <wrl.h>
+#include <d3d12.h>
+#include <dxgi1_5.h>
+#include <d3dcommon.h>
 #include <Windows.h>
+
+#include <string>
 
 namespace ddn
 {
 
 void ValidateResult(HRESULT hr);
+
+Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(const std::string& source, const std::string& entry_point, const std::string& compiler_target);
+
+Microsoft::WRL::ComPtr<IDXGIFactory5> CreateFactory();
+
+Microsoft::WRL::ComPtr<ID3D12Device> GetDevice(ID3D12DeviceChild& device_child);
 
 }  // namespace ddn
